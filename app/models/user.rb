6116e05864defddba0f04f331b3e4094609b1085
huize-fumi <arin0ma2@yahoo.co.jp>
2021-03-25 11:13:45 +0900

    新規登録(ユーザー情報)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,       presence: true
+  validates :email,          presence: true, uniqueness: true
+  validates :password,       presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+  validates :last_name,      presence: true
+  validates :first_name,     presence: true
+  validates :last_name_ruby, presence: true
+  validates :first_name_ruby,presence: true
+
 end
