41e39bb456c703b17597dc7e8753be73ff58f468
huize-fumi <arin0ma2@yahoo.co.jp>
2021-03-26 12:00:50 +0900

    with_options修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,       presence: true
-  validates :email,          presence: true, uniqueness: true, length: { minimum: 6 }
-  template = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password,       presence: true, format: { with: template }
-  validates :last_name,      presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :first_name,     presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
 
   with_options presence: true do
-    validates :last_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/ }
-  end
+    validates :nickname
+    validates :email, uniqueness: true, length: { minimum: 6 }
+    validates :birthday
 
-  validates :birthday, presence: true
+    template = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates :password, format: { with: template }
+
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_ruby
+      validates :first_name_ruby
+    end
+  end
 end
