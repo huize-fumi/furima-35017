040d16473aec882718b7c8f2effecd2df48e7326
huize-fumi <arin0ma2@yahoo.co.jp>
2021-03-30 16:02:58 +0900

    editアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root to: "items#index"
   devise_for :users
-  resources :items, only:[:index, :new, :create, :show]
+  resources :items, only:[:index, :new, :create, :show, :edit]
 
 end
